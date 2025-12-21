e5e44ce618c13134a146310afee80c4fe451405f
Justin L. Wells <justinleewells@icloud.com>
2025-12-21 15:02:02 -0600

    Implement rudimentary map rendering

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,6 +1,8 @@
 import Player from '@components/player'
 
 import PlayerImage from '@images/player.png'
+import ElvenImage from '@images/elven.png'
+import ExampleJson from '@tiled/example.json'
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -11,9 +13,18 @@ export default class GameScene extends Phaser.Scene {
       frameWidth: 16,
       frameHeight: 16,
     })
+    this.load.image('elven', ElvenImage)
+    this.load.tilemapTiledJSON('map', ExampleJson)
   }
   create() {
-    this.player = new Player(this, 0, 0)
+    const map = this.make.tilemap({ key: 'map' })
+
+    const tiles = map.addTilesetImage('elven')
+    const ground = map.createLayer(0, tiles, 0, 16)
+    const mask = map.createLayer(1, tiles, 0, 16)
+    const objects = map.createLayer(2, tiles, 0, 16)
+
+    this.player = new Player(this, 0, 16)
     this.add.existing(this.player)
   }
 }
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default {
       '@images': path.resolve(__dirname, 'src/assets/images'),
       '@scenes': path.resolve(__dirname, 'src/scenes'),
       '@utils': path.resolve(__dirname, 'src/utils'),
+      '@tiled': path.resolve(__dirname, 'tiled'),
     },
   },
 }
